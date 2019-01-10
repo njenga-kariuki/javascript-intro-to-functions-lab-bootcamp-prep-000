95f2911fe07c99e84fa20732078014143c014fda
Njenga Kariuki <njengak@me.com>
2019-01-10 00:42:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ function logWhisper(string) {
 function  sayHitoGrandma(string) {
   if (string === string.toUpperCase) {
     return "YES INDEED"
-  }
-  else if (string === string.toLowerCase) {
+  } else if (string === string.toLowerCase) {
     return "I can't hear you"
-  }
+  } else if (string === "I love you, Grandma") {
+    return "I love you, too"
+  } else 
 }
